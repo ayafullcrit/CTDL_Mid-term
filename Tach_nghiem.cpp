f7d0ad6fa6a96abc3dbae45ab0f226dc2ad0a81c
ayafullcrit <nguyendangphong2k6@gmail.com>
2025-04-12 09:16:07 +0700

    Update Tach_nghiem.cpp

--- Tach_nghiem.cpp
+++ Tach_nghiem.cpp
@@ -1,98 +1,154 @@
-#include <iostream>
-#include <iomanip>
-#include <vector>
-#include <sstream>
-#include <string.h>
-#include <fstream>
-#define FOR(i, a, b) for (int i = a; i <= b; i++)
+#include<iostream>
+#include<string>
 using namespace std;
-void Print(float f[101][101], int N, int M, string VariableCol[], string VariableRow[])
+struct Nguoi{
+    string ID;
+    string HoTen;
+    string MaChungKhoan;
+    int SoCoPhieu;
+    Nguoi* next;
+    Nguoi* prev;
+};
+typedef Nguoi* nguoi;
+nguoi head = NULL;
+nguoi tail = NULL;
+nguoi TaoNguoi(string id, string hoten, string macp, int socp)
 {
-    cout << " ";
-    FOR(k, 1, (N + M + 2) * 11 - 1)
-    cout << "_";
-    cout << endl;
-    cout << "|" << setw(10);
-    FOR(i, 0, N + M)
-    {
-        cout << VariableCol[i];
-        cout << "|" << setw(10);
-    }
-    cout << setw(12) << "Value|\n";
-    VariableRow[M + 1] = "Z";
-    FOR(i, 1, M + 2)
-    {
-        cout << "|";
-        int uCount = 0;
-        FOR(k, 1, (N + M + 2) * 11)
+    nguoi p = new Nguoi;
+    p->ID = id;
+    p->HoTen = hoten;
+    p->MaChungKhoan = macp;
+    p->SoCoPhieu = socp;
+    p->next = NULL;
+    p->prev = NULL;
+    return p;
+}
+void NhapDanhSach(){
+    int n;
+    cout << "Nhap so luong nguoi: ";
+    cin >> n;
+    for(int i = 1; i <= n; i++){
+        string id, hoten, macp;
+        int socp;
+        cout << "Nhap nguoi thu " << i << ":\nNhap CCCD: ";
+        cin >> id;
+        cout << "Nhap Ho Ten: ";
+        cin.ignore();
+        getline(cin, hoten);
+        cout << "Nhap Ma Chung Khoan: ";
+        cin >> macp;
+        cout << "Nhap So Co Phieu: ";
+        cin >> socp;
+        nguoi p = TaoNguoi(id, hoten, macp, socp);
+        if(head == NULL){
+            head = p;
+            tail = p;
+        }
+        else
         {
-            if (uCount == 10)
+            tail->next = p;
+            p->prev = tail;
+            tail = p;
+        }
+        cout << endl;
+    }
+}
+void LietKeTheoMaCK()
+{
+    string MaCK;
+    cout << "Nhap ma chung khoan: ";
+    cin >> MaCK;
+    nguoi p = head;
+    while(p != NULL)
+    {
+        if(p->MaChungKhoan == MaCK)
+        {
+            cout << "CCCD: " << p->ID << ", Ho Ten: " << p->HoTen << ", So Co Phieu: " << p->SoCoPhieu << endl;
+        }
+        p = p->next;
+    }
+}
+void CoPhieuCaoNhat()
+{
+    int max = 0;
+    nguoi p = head;
+    while(p != NULL)
+    {
+        if(p->SoCoPhieu > max)
+        {
+            max = p->SoCoPhieu;
+        }
+        p = p->next;
+    }
+    p = head;
+    cout << endl << "So co phieu cao nhat: " << max << endl;
+    cout << "Danh sach nguoi co so co phieu cao nhat: " << endl;
+    while(p != NULL)
+    {
+        if(p->SoCoPhieu == max)
+        {
+            cout << "CCCD: " << p->ID << ", Ho Ten: " << p->HoTen << ", So Co Phieu: " << p->SoCoPhieu << endl;
+        }
+        p = p->next;
+    }
+}
+void XoaCCCD()
+{
+    string id;
+    cout << endl << "Nhap CCCD can xoa: ";
+    cin >> id;
+    nguoi p = head;
+    while(p != NULL)
+    {
+        if(p->ID == id)
+        {
+            if(p->prev == NULL) // Xoa phan tu dau
             {
-                cout << '|';
-                uCount = 0;
-                continue;
+                head = p->next;
             }
-            uCount++;
-            cout << '-';
-        }
-        if (i == M + 2)
+            else if(p->next == NULL) // Xoa phan tu cuoi
+            {
+                tail = p->prev;
+                tail->next = NULL;
+            }
+            else // Xoa phan tu o giua
+            {
+                p->prev->next = p->next;
+                p->next->prev = p->prev;
+            }
             break;
-        cout << endl;
-        cout << "|" << setw(10) << VariableRow[i] << "|" << setw(10);
-
-        FOR(j, 1, N + M + 1)
-        {
-            cout << f[i][j] << "|";
-            if (j <= N + M)
-                cout << setw(10);
         }
-        cout << endl;
+        p = p->next;
+        nguoi temp = p->next;
     }
-    cout << endl
-         << endl;
+    cout << "Da xoa nguoi co CCCD: " << id << endl;
+    cout << "Danh sach nguoi sau khi xoa: " << endl;
+    p = head;
+    while(p!=NULL)
+    {
+        cout << "CCCD: " << p->ID << ", Ho Ten: " << p->HoTen << ", So Co Phieu: " << p->SoCoPhieu << endl;
+        p = p->next;
+    }
+    cout << endl;
 }
 int main()
 {
-    //file input
-    string FileName;
-    cout << "Nhap ten file input: ";
-    cin >> FileName;
-    freopen(FileName.c_str(), "r", stdin);
-    
-    int x;
-    cin >> x;
-    cout << x;
+    NhapDanhSach();
+    LietKeTheoMaCK();
+    CoPhieuCaoNhat();
+    XoaCCCD();
 }
-
 /*
-2
-2
-3 5
-x2 <= 4
-x1 <= 3
-
-2
-2
-3 5
-x1 + 2x2 <= 6
-3x1 + 2x2 <= 12
-
-2
-2
-40 30
-2x1 + x2 <= 16
-x1 + x2 <= 9
-
-3
-2
-3 5
--2x1 + -4x2 >= -40
-3x1 + 2x2 <= 36
-6x1 + 5x2 <= 60
-
-2
-2
-8 6
-4x1 + 2x2 <= 60 
-2x1 + 4x2 <= 48
+123456789012
+Nguyen Van A
+VIC
+10000
+234567890123
+Tran Thi B
+VHM
+15000
+345678901234
+Le Van C
+VIC
+20000
 */
